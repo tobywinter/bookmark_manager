fd5f597a7d087fa8d759fdfca1ac859ef6674d20
Sam Pritchard <sampritchard@msn.com>
2017-05-31 17:50:01 +0100

    update datamapper setup

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,7 @@ include DataMapper::Resource
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+
 DataMapper.finalize
 DataMapper.auto_upgrade!
