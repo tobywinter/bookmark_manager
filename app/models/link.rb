249a0fc7207b38aaf54ba31945d483bffec601df
Sam Pritchard <sampritchard@msn.com>
2017-05-31 17:00:32 +0100

    Challenge 13
    - distinguished between development and test environments
    - test with test database
    - dev with dev database, so links added persist

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,6 @@ include DataMapper::Resource
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
