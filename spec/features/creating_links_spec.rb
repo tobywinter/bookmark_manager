fe431885745b7a7d990f55484f15ef136b8dd30b
toby <toby.hw@live.co.uk>
2017-05-31 15:09:49 +0100

    Challenge 11
    Adds
    - Feature test for saving links to the list

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,18 @@
+# As a User
+# So I can save links for later
+# I want to add links to the list
+
+feature 'I want to save links to the list' do
+  scenario 'use a form to save a link to the list' do
+    visit('/links/new')
+    fill_in 'url', with: "http://www.google.co.uk/"
+    fill_in 'title', with: 'This is Google'
+    click_button 'Save link'
+
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content('This is Google')
+    end
+  end
+end
