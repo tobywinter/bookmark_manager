0fe7633fb18fe4e034886d4c4ef427b4044f7315
toby <toby.hw@live.co.uk>
2017-06-01 17:00:58 +0100

    Challenge 17
    Adds
    Feature test for adding multiple tags
    Functionality to add multiple tags (each tag seperated by ', ')

--- app/app.rb
+++ app/app.rb
@@ -15,8 +15,9 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+    params[:tags].split(', ').each do |tag|
+      link.tags << Tag.first_or_create(name: tag)
+    end
     link.save
     redirect '/links'
   end
--- spec/features/adding_tag_spec.rb
+++ spec/features/adding_tag_spec.rb
@@ -13,4 +13,15 @@ feature 'Adding tags to links' do
     link = Link.first
     expect(link.tags.map(&:name)).to include('Search Engine')
   end
+
+  scenario 'Adds multiple tags to a link' do
+    visit '/links/new'
+    fill_in 'url', with: "http://www.zombo.com/"
+    fill_in 'title', with: 'Zombocom'
+    fill_in 'tags', with: 'zombo, welcome'
+
+    click_button 'Save Link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('zombo', 'welcome')
+  end
 end
